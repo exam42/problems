735d26e547d19fc8c151867458297cc63c7ce18c
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-13 15:47:23 +0530

    keep to simple pattern

--- py/sum/__init__.py
+++ py/sum/__init__.py
@@ -12,22 +12,23 @@ def exists():
 @check50.check(exists)
 def sum_2_3():
     """sum_2_3"""
-    test_input_output("python3 sum.py", "sum_2_3_input.txt", "sum_2_3_output.txt")
+    test_input_output("sum_2_3_input.txt", "sum_2_3_output.txt")
 
 
 @check50.check(exists)
 def sum_20_22():
     """sum_20_22"""
-    test_input_output("python3 sum.py", "sum_20_22_input.txt", "sum_20_22_output.txt")
+    test_input_output("sum_20_22_input.txt", "sum_20_22_output.txt")
 
 
 @check50.check(exists)
 def sum_10_20():
     """sum_10_20"""
-    test_input_output("python3 sum.py", "sum_10_20_input.txt", "sum_10_20_output.txt")
+    test_input_output("sum_10_20_input.txt", "sum_10_20_output.txt")
 
 
 # Helpers
 def test_input_output(executable, input_file, output_file):
     """A function to test a single input/output pair"""
+    executable = "python3 sum.py"
     check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
