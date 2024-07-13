42a0571f261b4a1032c2c7c04a966a5ac527c4a0
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-13 13:45:55 +0530

    Checks for Sum of numbers in Python

---
+++ py/sum/__init__.py
@@ -0,0 +1,33 @@
+
+import check50
+
+@check50.check()
+def exists():
+    """sum.py exists"""
+    check50.exists("sum.py")
+    check50.include("sum_2_3_input.txt", "sum_2_3_output.txt")
+    check50.include("sum_20_22_input.txt", "sum_20_22_output.txt")
+    check50.include("sum_10_20_input.txt", "sum_10_20_output.txt")
+
+@check50.check(exists)
+def sum_2_3():
+    """sum_2_3"""
+    test_input_output("python3 sum.py", "sum_2_3_input.txt", "sum_2_3_output.txt")
+
+
+@check50.check(exists)
+def sum_20_22():
+    """sum_20_22"""
+    test_input_output("python3 sum.py", "sum_20_22_input.txt", "sum_20_22_output.txt")
+
+
+@check50.check(exists)
+def sum_10_20():
+    """sum_10_20"""
+    test_input_output("python3 sum.py", "sum_10_20_input.txt", "sum_10_20_output.txt")
+
+
+# Helpers
+def test_input_output(executable, input_file, output_file):
+    """A function to test a single input/output pair"""
+    check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
