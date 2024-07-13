f436e18098ff6bbc97c704e33915c60e7c1fc2bd
Rohini Kumar Barla <rohinibarla@enhance42.com>
2024-07-13 16:50:11 +0530

    Checks Common Prime Factors in C

--- sum/__init__.py
+++ sum/__init__.py
@@ -19,22 +19,23 @@ def compiles():
 @check50.check(compiles)
 def sum_2_3():
     """sum_2_3"""
-    test_input_output("./sum", "sum_2_3_input.txt", "sum_2_3_output.txt")
+    test_input_output("sum_2_3_input.txt", "sum_2_3_output.txt")
 
 
 @check50.check(compiles)
 def sum_20_22():
     """sum_20_22"""
-    test_input_output("./sum", "sum_20_22_input.txt", "sum_20_22_output.txt")
+    test_input_output("sum_20_22_input.txt", "sum_20_22_output.txt")
 
 
 @check50.check(compiles)
 def sum_10_20():
     """sum_10_20"""
-    test_input_output("./sum", "sum_10_20_input.txt", "sum_10_20_output.txt")
+    test_input_output("sum_10_20_input.txt", "sum_10_20_output.txt")
 
 
 # Helpers
-def test_input_output(executable, input_file, output_file):
+def test_input_output(input_file, output_file):
     """A function to test a single input/output pair"""
+    executable = "./sum"
     check50.run(executable).stdin(open(input_file).read(), prompt=False).stdout(open(output_file).read(), regex=False).exit()
